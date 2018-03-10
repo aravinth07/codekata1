790b27850b997279ae233edb348e140ccba22ad4
aravinth07 <37236641+aravinth07@users.noreply.github.com>
2018-03-10 15:24:30 +0530

    Add files via upload

---
+++ python/bigenner/py2.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rgcet
+#
+# Created:     10/03/2018
+# Copyright:   (c) rgcet 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+a=int(input("enter number"))
+
+if a==0:
+    print("zero")
+elif a<0:
+    print("negative")
+else :
+    print("positive")

e6ab9d961e1922cfea41163e36f1c68e74907a82
aravinth07 <37236641+aravinth07@users.noreply.github.com>
2018-03-10 14:52:43 +0530

    Add files via upload

---
+++ python/bigenner/py1.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rgcet
+#
+# Created:     10/03/2018
+# Copyright:   (c) rgcet 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import math
+number=raw_input()
+if(number.isnumeric()):
+    print("u entered a number")
+    number=int(number)
+    if number%2 ==0 and number !=0:
+        print("even number")
+    else:
+        print("odd number")
+else:
+    print("invalid number")

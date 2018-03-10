65506ca19bea07fcc4f59c2bd191a0d080f93bb6
aravinth07 <37236641+aravinth07@users.noreply.github.com>
2018-03-10 15:35:07 +0530

    Add files via upload

---
+++ python/bigenner/py3.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rgcet
+#
+# Created:     10/03/2018
+# Copyright:   (c) rgcet 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+list=['a','e','i,','o','u']
+input=raw_input()
+if input in list:
+    print("vowel")
+else:
+    print("consonant")

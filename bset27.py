3c5d98be7d4c86c2829c6e5d50c6ae5bb39badfb
aravinth07 <37236641+aravinth07@users.noreply.github.com>
2019-07-24 19:16:36 +0530

    Update bset27.py

--- bset27.py
+++ bset27.py
@@ -1,11 +1,11 @@
-num = int(input())
-sum = 0
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
-if num == sum:
+n = int(input())
+a = 0
+t = n
+while t > 0:
+   digit = t % 10
+   a += digit ** 3
+   t //= 10
+if n == a:
    print("yes")
 else:
    print("no")
